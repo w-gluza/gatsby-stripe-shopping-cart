04d9eb319a5e0a7a0b42a59a2f261dbc7917008c
Wioletta Gluza <wiolagluza@gmail.com>
2020-06-01 22:22:05 +0200

    success and cancel components

---
+++ src/pages/cancel.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const cancel = () => {
+  return (
+    <div>
+      <p>Unfortunately your payment attempt was not successful.</p>
+    </div>
+  )
+}
+
+export default cancel
---
+++ src/pages/success.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const success = () => {
+  return (
+    <div>
+      <p>Your payment has been successfully processed. Thank you.</p>
+    </div>
+  )
+}
+
+export default success
